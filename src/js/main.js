1251979cabb722b6cd8e11e18c73a6d0ef58fa10
benardoneka <benardoneka@gmail.com>
2022-07-15 20:31:51 +0300

    local storage handled, check the city

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,19 @@
+var newName1 = document.getElementById("cityInput");
+var cityName = document.getElementById("cityName");
 function WeatherUserInfo() {
-    
     var newName = document.getElementById("cityInput");
-    var cityName = document.getElementById("cityName");
-    cityName.innerHTML = newName.value;
+// console.log(newName)
 
     // CURRENT WEATHER 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=`+newName.value+`&units=metric&APPID=93022b80d2fa47743474256c2ea49a47`)
     .then(response => response.json())
     .then(current => {
         document.getElementById('currentImg').setAttribute('src', "https://openweathermap.org/img/w/" + current.weather[0].icon + ".png");
-        document.getElementById('current').innerHTML = current.weather[0].description;
-        document.getElementById('temp').innerHTML = current.main.temp + " °C";
-        document.getElementById('wind').innerHTML = current.wind.speed + ' m/s';
-        document.getElementById('humid').innerHTML = current.main.humidity + ' &percnt;';
-        console.log(current)
+        document.getElementById('current').value = current.weather[0].description;
+        document.getElementById('temp').value = current.main.temp + " °C";
+        document.getElementById('wind').value = current.wind.speed + ' m/s';
+        document.getElementById('humid').value = current.main.humidity + ' &percnt;';
+        // console.log(current)
     })
 
     // FORECAST
@@ -28,7 +28,7 @@ function WeatherUserInfo() {
         data.list[i].weather[0].icon
         +".png";
     }
-    console.log(data)
+    // console.log(data)
 
 
     })
@@ -51,4 +51,58 @@ function WeatherUserInfo() {
         for(i = 0; i<5; i++){
             document.getElementById("day" + (i+1)).innerHTML = weekday[CheckDay(i)];
         }
-   
+
+
+//Saving Data
+
+// const  = document.getElementById('currentImg').setAttribute('src', "https://openweathermap.org/img/w/" + current.weather[0].icon + ".png");
+// const fname = document.getElementById("fname");
+// const lname = document.getElementById("lname");
+// const city = document.getElementById('cityName');
+const current = document.getElementById('current');
+const temp = document.getElementById('temp');
+const wind = document.getElementById('wind');
+const humid = document.getElementById('humid'); 
+
+const local = localStorage
+
+function saveData(){
+
+    let data = {
+        // "firstName": fname.value,
+        // "lastName": lname.value,
+        "city": newName.value,
+        "tempreture": temp.value,
+        "wind": wind.value,
+        "humid": humid.value
+    }
+
+    local.setItem(newName1.value, JSON.stringify(data));
+}
+
+// const getItem = document.getElementById("getName");
+
+function getData(){
+    const dataFromStorage = local.getItem(newName1.value);
+    const dataInfo = JSON.parse(dataFromStorage);
+    document.getElementById("city").innerHTML = dataInfo.city;
+    document.getElementById("tempr").innerHTML = dataInfo.tempreture;
+    document.getElementById("windy").innerHTML = dataInfo.wind;
+    document.getElementById("humidity").innerHTML = dataInfo.humid;    
+}
+
+function deleteData(){
+    const dataFromStorage = local.getItem(newName.value);
+    const dataInfo = JSON.parse(dataFromStorage);
+    
+
+
+    for (i = 0; i < dataInfo.length; i++){
+        document.getElementById("del").innerHTML = dataInfo
+        if (newName == dataInfo[i]){
+            dataInfo.removeItem()
+        }else {
+            console.log("Item not found...")
+        }
+    }
+}
